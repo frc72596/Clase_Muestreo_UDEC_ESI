11353c66f89067178451533b78a8aec8e3a12946
Your Name <feramirezco@unal.edu.co>
2025-03-28 09:08:59 -0500

    cambios 2

--- Codigo_Clase.R
+++ Codigo_Clase.R
@@ -1,1 +1,7 @@
-#PRUEA
+# Clase de muestreo
+# link: https://www.datos.gov.co/Salud-y-Protecci-n-Social/MORBILIDAD-POR-CANCER/utgq-6fdm/about_data
+# Casos de cancer en Pereira, Risaralda- Descargar para csv para excel
+
+
+
+
